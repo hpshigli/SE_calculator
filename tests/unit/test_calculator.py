5872655ebf26698505d178618cb1c7dacc04c368
hpshigli <hardik.shigli@gmail.com>
2025-09-29 12:24:27 +0530

    second commit

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -34,3 +34,12 @@ class TestMultiplyDivideWithValidation:
             divide("10", 2)
 
 # TODO: Students will add TestMultiplyDivide class
+    def test_add_negative_numbers(self):
+
+        assert add(-1, -1) == -2 
+        assert add(-5, 3) == -2
+
+    def test_subtract_negative_numbers(self): 
+
+        assert subtract(-1, -1) == 0 
+        assert subtract(-5, -3) == -2
